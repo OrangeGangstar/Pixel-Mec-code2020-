d0ff269e3b50bba3869758e2b9281913a993921b
AlamPixel <35508714+AlamPixel@users.noreply.github.com>
2020-02-29 16:13:12 -0600

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -7,14 +7,20 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.Timer; 
 import edu.wpi.first.cameraserver.*;
+import edu.wpi.first.wpilibj.util.Color;
+
+import com.revrobotics.ColorSensorV3;		//these import will not work without the rev robotics libraries that needed to be installed
+import com.revrobotics.ColorMatchResult;	//to vscode and can be found on rev robotics color sensor page: http://www.revrobotics.com/rev-31-1557/
+import com.revrobotics.ColorMatch;
 
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  private static final String Blue_1 = "Blue_1";
+  private static final String Red_1 = "Red_1";
+  private String AutoSelected;
+  private final SendableChooser<String> AutoChooser = new SendableChooser<>();
 
   private static final int legTopLeft = 2;   //setting up wheel motors to their PMW ports on the RobotRIO
   private static final int legBottomLeft = 3;
@@ -24,11 +30,12 @@ public class Robot extends TimedRobot {
   private static final int EXHALE = 5;
   private static final int mom = 6;
 
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
+
   private static final int gamer = 0; //sets up joystick to connect to usb port 1 on the laptop/computer
 
   private static final double USonHoldDist = 12.0; //ultrasonic sensor limited before it stops the robot(in inches)
   private static final double MathValToDist = 0.125; //set value used to convert sensor values to inches
-  private static final double speedConst = 0.05; //set constant speed value for robot using ultrasonic
   private static final int UsonPort = 0; //ultrasonic analog port (aka Analog In on the RobotRIO)
 
   private final AnalogInput AUsonIn = new AnalogInput(UsonPort); //gives the ultrasonic sensor a name
@@ -43,6 +50,14 @@ public class Robot extends TimedRobot {
   PWMVictorSPX craftsmanBLOW = new PWMVictorSPX(EXHALE);
   PWMVictorSPX FRICK = new PWMVictorSPX(mom);
 
+  private final ColorSensorV3 chop = new ColorSensorV3(i2cPort);
+  private final ColorMatch reeves = new ColorMatch();
+
+  private final Color BlueBoi = ColorMatch.makeColor(0.143, 0.427, 0.429);
+  private final Color GreenBoi = ColorMatch.makeColor(0.197, 0.561, 0.240);
+  private final Color RedBoi = ColorMatch.makeColor(0.561, 0.232, 0.114);
+  private final Color YellowBoi = ColorMatch.makeColor(0.361, 0.524, 0.113);
+  
   Timer clock = new Timer();
 
   /**
@@ -51,13 +66,12 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
-
+    AutoChooser.setDefaultOption("Blue_1", Blue_1);
+    AutoChooser.addOption("Red_1", Red_1);
+    SmartDashboard.putData("Auto choices", AutoChooser);
+    
     TopL.setInverted(false); //flips the left side of motors for wheels
     BottomL.setInverted(false);//false cause... not needed this year
-
 
     MecPixel = new MecanumDrive(TopL, BottomL, TopR, BottomR); //hooks up the drive train with the PMW motors
                                                                //that are linked to the wheels
@@ -65,6 +79,11 @@ public class Robot extends TimedRobot {
 
     CameraServer.getInstance().startAutomaticCapture();
     CameraServer.getInstance().startAutomaticCapture();
+
+    reeves.addColorMatch(BlueBoi);
+    reeves.addColorMatch(GreenBoi);
+    reeves.addColorMatch(RedBoi);
+    reeves.addColorMatch(YellowBoi);
   }
 
   /**
@@ -77,7 +96,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
-    double currentDist = AUsonIn.getValue() - 220;
+    double currentDist = AUsonIn.getValue() ;
     System.out.println(currentDist);
     //System.out.println("asfsdfsdfsd");
     //System.out.println(xylophone.xJoy());
@@ -96,9 +115,10 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
+    AutoSelected = AutoChooser.getSelected();
+    AutoSelected = SmartDashboard.getString("Auto Selector", Blue_1);
+    System.out.println("Auto selected: " + AutoSelected);
+
     clock.reset();
 		clock.start();
   }
@@ -108,29 +128,26 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
+    switch (AutoSelected) {
+      case Blue_1:
         // Put custom auto code here
         break;
-      case kDefaultAuto:
+      case Red_1:
       default:
-      
-      if (clock.get() < 10.0) {
-        MecPixel.driveCartesian(0.0, 0.7, 0.0,0); // drive forwards half speed
-        }
-      else {
-        MecPixel.driveCartesian(0.0, 0.0, 0.0, 0.0);
-      }
+          if (clock.get() < 10.0) {
+            MecPixel.driveCartesian(0.0, 0.7, 0.0,0); // drive forwards half speed
+            }
+          else {
+            MecPixel.driveCartesian(0.0, 0.0, 0.0, 0.0);
+          }
         break;
 
     }
   }
 
-
   // This function is called periodically during operator control.
   @Override
   public void teleopPeriodic() {
-
 
     yValue yylophone = new yValue(gStick.getY());
     xValue xylophone = new xValue(gStick.getX());
@@ -138,8 +155,34 @@ public class Robot extends TimedRobot {
 
     Fricker Shaquille = new Fricker(gStick.getThrottle());
 
+    Color pewach = chop.getColor();
+    String colorString;
+    ColorMatchResult match = reeves.matchClosestColor(pewach);
+
+    if(match.color == BlueBoi){
+      colorString = "BLUE";
+    }
+    else if(match.color == RedBoi){
+      colorString = "RED";
+    }
+    else if(match.color == GreenBoi){
+      colorString = "GREEN";
+    }
+    else if(match.color == YellowBoi){
+      colorString = "YELLOW";
+    }
+    else{
+      colorString = "UNKNOWN";
+    }
+
+    SmartDashboard.putNumber("Red", pewach.red);
+    SmartDashboard.putNumber("Green", pewach.green);
+    SmartDashboard.putNumber("Blue", pewach.blue);
+    SmartDashboard.putNumber("Confidence", match.confidence);
+    SmartDashboard.putString("Detected Color", colorString);
+
     MecPixel.driveCartesian(xylophone.xJoy(), yylophone.yJoy(), zylophone.zJoy(), 0.0); //sets driving to run using 
-                                                                                      //joystick controls
+                                                                                        //joystick controls
     
     FRICK.set(Shaquille.fThot());
 
@@ -154,8 +197,38 @@ public class Robot extends TimedRobot {
     if(gStick.getRawButton(1) == true){
         craftsmanBLOW.set(0.7);
     }
+    else if(gStick.getRawButton(3) == true){
+        craftsmanBLOW.set(0.2);
+    }
     else{
         craftsmanBLOW.set(0.0);
+    }
+
+    if(gStick.getRawButton(9) == true){
+      while(match.color != BlueBoi){
+        pewach = chop.getColor();
+        match = reeves.matchClosestColor(pewach);
+        FRICK.set(0.5);   
+        if(match.color == BlueBoi){
+          colorString = "BLUE";
+        }
+        else if(match.color == RedBoi){
+          colorString = "RED";
+        }
+        else if(match.color == GreenBoi){
+          colorString = "GREEN";
+        }
+        else if(match.color == YellowBoi){
+          colorString = "YELLOW";
+        }
+        else{
+          colorString = "UNKNOWN";
+        }
+        SmartDashboard.putString("Detected Color", colorString);
+      }
+    }
+    else{
+      FRICK.set(0.0);
     }
 
     Timer.delay(0.005);    //timer sets up the code to have a 1 millisecond delay to avoid overworking and 
